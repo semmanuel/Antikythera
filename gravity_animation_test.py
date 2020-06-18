b1042f37cd7adcfaa39e01e13d57cd14b9b80169
cartertrafton <cartertrafton@gmail.com>
2020-06-18 17:50:23 -0400

    added trails & button

--- gravity_animation_test.py
+++ gravity_animation_test.py
@@ -12,6 +12,11 @@ import time
 # initiate pygame and clock
 pygame.init()
 clock = pygame.time.Clock()
+game_font = pygame.font.SysFont('consolas', 20)
+
+# dimensions
+WIDTH = 1000
+HEIGHT = 600
 
 # gravitational constant
 g = 0.4
@@ -21,24 +26,35 @@ BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 RED = (255, 0, 0)
+WHITE = (255, 255, 255)
 
 # set up surface plane
-surface = pygame.display.set_mode((600, 600))  # ((width, height))
+surface = pygame.display.set_mode((WIDTH, HEIGHT))  # ((width, height))
 pygame.display.set_caption('animation test')
-
 surface.fill(BLACK)
 
+# trails
+global trails_active
+trails_active = False
+
+# trails button
+trails_button = pygame.Rect(0, 0, 100, 50)
+trails_button_surface = game_font.render("TRAILS", True, (0, 0, 0))
+
+pygame.draw.rect(surface, WHITE, trails_button)
+surface.blit(trails_button_surface, (50, 50))
 
 ############################## celestial body object
 class Body(object):
-    def __init__(self, m, x, y, c):
+    def __init__(self, m, x, y, r, c):
         # mass, postion (x, y), color
         self.mass = m
         self.position = np.array([x, y])
         self.last_position = np.array([x, y])
         self.velocity = np.array([0, 0])
-        self.accel = np.array([0, 0])
+        self.accel = np.array([randint(-1,1), randint(-1,1)])
         self.color = c
+        self.radius = r
 
     def applyForce(self, force):
         # apply forces to a body
@@ -54,16 +70,14 @@ class Body(object):
 
     def display(self):
         # draw over old object location
-        pygame.draw.circle(surface, BLACK, (int(self.last_position[0]), int(self.last_position[1])),
-                           10)  # (drawLayer, color, (coordinates), radius)
+        pygame.draw.circle(surface, BLACK, (int(self.last_position[0]), int(self.last_position[1])), self.radius)  	# (drawLayer, color, (coordinates), radius)
 
         # draw trail (Comment this line out to remove trails)
-        pygame.draw.circle(surface, RED, (int(self.last_position[0]), int(self.last_position[1])),
-                           10)  # (drawLayer, color, (coordinates), radius)
+        if trails_active == True:
+            pygame.draw.line(surface, RED, (int(self.last_position[0]), int(self.last_position[1])), (int(self.position[0]), int(self.position[1])), 5)
 
         # draw new object location
-        pygame.draw.circle(surface, self.color, (int(self.position[0]), int(self.position[1])),
-                           10)  # (drawLayer, color, (coordinates), radius)
+        pygame.draw.circle(surface, self.color, (int(self.position[0]), int(self.position[1])), self.radius)
 
     # print("x:" + str(self.position[0]) + " ,  y:" + str(self.position[1]) )
 
@@ -81,18 +95,17 @@ class Body(object):
 ############################## set up and draw
 def setup():
     # sun
-    body1 = Body(100, 300, 300, YELLOW)
+    body1 = Body(500, WIDTH/2, HEIGHT/2, 10, YELLOW)
 
     # planets
-    body2 = Body(10, randint(0, 600), randint(0, 600), BLUE)
-    body3 = Body(5, randint(0, 600), randint(0, 600), BLUE)
-    body4 = Body(1, randint(0, 600), randint(0, 600), BLUE)
-    body5 = Body(8, randint(0, 600), randint(0, 600), BLUE)
+    body2 = Body(randint(0, 10), randint(0, WIDTH), randint(0, HEIGHT), 5, BLUE)
+    body3 = Body(randint(0, 10), randint(0, WIDTH), randint(0, HEIGHT), 5, BLUE)
+    body4 = Body(randint(0, 10), randint(0, WIDTH), randint(0, HEIGHT), 5, BLUE)
+    body5 = Body(randint(0, 10), randint(0, WIDTH), randint(0, HEIGHT), 5, BLUE)
 
     # list of all bodies in universe
     global bodies
     bodies = [body1, body2, body3, body4, body5]
-
     return
 
 
@@ -106,6 +119,10 @@ def draw():
                 body.applyForce(force)
         body.update()
         body.display()
+
+    pygame.draw.rect(surface, WHITE, trails_button)
+    surface.blit(trails_button_surface, (0, 0))
+
     return
 
 
@@ -131,8 +148,22 @@ if __name__ == "__main__":
     while True:
         # draw bodies to screen
         draw()
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                run = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos
+                # trails button
+                if trails_button.collidepoint(mouse_pos):
+                    print("trails button pushed")
+                    if trails_active == True:
+                        trails_active = False
+                        surface.fill(BLACK)
+
+                    else:
+                        trails_active = True
+
         pygame.display.update()
         time.sleep(0.05)
