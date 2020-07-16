55967f13b42ee92718d5bd1e7dfc13328dd7bd83
semmanuel <stanleyemmanuel11@gmail.com>
2020-07-16 14:13:40 -0400

    comments added for asteroid potential impacts

--- database_creation/create_db.py
+++ database_creation/create_db.py
@@ -13,13 +13,13 @@ lunareclipse.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
 solareclipse= read_csv("solar.csv", encoding="ISO-8859-1")
 sqlite_table = "Solar Eclipse"
 solareclipse.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
- #Asteroids
+#Asteroids
 asteroid= read_csv("impacts.csv", encoding="ISO-8859-1")
 engine = create_engine('sqlite:///Asteroid.db', echo=True)
 sqlite_connection = engine.connect()
 sqlite_table = "Potential Asteroid impacts"
 asteroid.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
-
+#Asteroid Magnitude & Diameter are present in table
 asteroid= read_csv("orbits.csv", encoding="ISO-8859-1")
 sqlite_table = "Asteroid Orbits"
 asteroid.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
