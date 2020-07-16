40563889e56122e8662bcde42ed80f17dbfb2e97
semmanuel <stanleyemmanuel11@gmail.com>
2020-07-16 14:06:41 -0400

    Created events database instead of separate eclipse relations in eclipse database, Removed Comets database and added comets relation to celestial objects database

--- database_creation/create_db.py
+++ database_creation/create_db.py
@@ -2,26 +2,10 @@ from pandas import read_csv
 from sqlalchemy import create_engine
 #this will convert csv files into a dataframe which it will be added to sqlalchemy and that will add a sqlite db
 
-comets= read_csv("near-earth-comets.csv", encoding="ISO-8859-1")
-
-engine = create_engine('sqlite:///comets.db', echo=True)
-sqlite_connection = engine.connect()
-sqlite_table = "COMETS"
-comets.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
-
-'''
-#Meteor Table
-= read_csv("near-earth-comets.csv", encoding="ISO-8859-1")
-
-engine = create_engine('sqlite:///meteorites.db', echo=True)
-sqlite_connection = engine.connect()
-sqlite_table = "METEORITES"
-meteorites.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
-'''
 #Eclipses Relation
 lunareclipse= read_csv("lunar.csv", encoding="ISO-8859-1")
 
-engine = create_engine('sqlite:///eclipses.db', echo=True)
+engine = create_engine('sqlite:///celestial_events.db', echo=True)
 sqlite_connection = engine.connect()
 sqlite_table = "Lunar Eclipse"
 lunareclipse.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
@@ -31,7 +15,6 @@ sqlite_table = "Solar Eclipse"
 solareclipse.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
  #Asteroids
 asteroid= read_csv("impacts.csv", encoding="ISO-8859-1")
-
 engine = create_engine('sqlite:///Asteroid.db', echo=True)
 sqlite_connection = engine.connect()
 sqlite_table = "Potential Asteroid impacts"
@@ -42,10 +25,14 @@ sqlite_table = "Asteroid Orbits"
 asteroid.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
 
 #Celestial Objects ~ Planets
-comets= read_csv("planets.csv", encoding="ISO-8859-1")
-
+planets= read_csv("planets.csv", encoding="ISO-8859-1")
 engine = create_engine('sqlite:///celestial_objects.db', echo=True)
 sqlite_connection = engine.connect()
 sqlite_table = "Planets"
+planets.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
+
+comets= read_csv("near-earth-comets.csv", encoding="ISO-8859-1")
+sqlite_table = "COMETS"
 comets.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
+
 sqlite_connection.close()
