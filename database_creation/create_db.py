d6741181cab5e3958edd98427cc071ae55031237
semmanuel <stanleyemmanuel11@gmail.com>
2020-07-16 01:55:38 -0400

    Extracted some database files to create eclipse(lunar and solar) and asteroid potential

--- database_creation/create_db.py
+++ database_creation/create_db.py
@@ -28,8 +28,18 @@ sqlite_connection = engine.connect()
 sqlite_table = "Lunar Eclipse"
 lunareclipse.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
 
-solareclipse= read_csv("lunar.csv", encoding="ISO-8859-1")
+solareclipse= read_csv("solar.csv", encoding="ISO-8859-1")
 sqlite_table = "Solar Eclipse"
 solareclipse.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
+ #Asteroids
+asteroid= read_csv("impacts.csv", encoding="ISO-8859-1")
 
+engine = create_engine('sqlite:///Asteroid.db', echo=True)
+sqlite_connection = engine.connect()
+sqlite_table = "Potential Asteroid impacts"
+asteroid.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
+
+asteroid= read_csv("orbits.csv", encoding="ISO-8859-1")
+sqlite_table = "Asteroid Orbits"
+asteroid.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
 sqlite_connection.close()
