784f0fd8474b534a0dc0b06260adb679484cd82b
semmanuel <stanleyemmanuel11@gmail.com>
2020-07-16 13:07:56 -0400

    Commented out meteorites table (need to replace csv)

--- database_creation/create_db.py
+++ database_creation/create_db.py
@@ -2,24 +2,22 @@ from pandas import read_csv
 from sqlalchemy import create_engine
 #this will convert csv files into a dataframe which it will be added to sqlalchemy and that will add a sqlite db
 
-'''comets= read_csv("near-earth-comets_1.csv", encoding="ISO-8859-1")
+comets= read_csv("near-earth-comets.csv", encoding="ISO-8859-1")
 
 engine = create_engine('sqlite:///comets.db', echo=True)
 sqlite_connection = engine.connect()
 sqlite_table = "COMETS"
 comets.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
 
-
-sqlite_connection.close()
 '''
 #Meteor Table
-meteorites= read_csv("near-earth-comets.csv", encoding="ISO-8859-1")
+= read_csv("near-earth-comets.csv", encoding="ISO-8859-1")
 
 engine = create_engine('sqlite:///meteorites.db', echo=True)
 sqlite_connection = engine.connect()
 sqlite_table = "METEORITES"
 meteorites.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
-
+'''
 #Eclipses Relation
 lunareclipse= read_csv("lunar.csv", encoding="ISO-8859-1")
 
