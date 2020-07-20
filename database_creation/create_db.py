f656daa298d88a4e9b6cf4f1f8174789d74fffa1
semmanuel <stanleyemmanuel11@gmail.com>
2020-07-20 11:46:58 -0400

    merge conflict resolved and added celestial events

--- database_creation/create_db.py
+++ database_creation/create_db.py
@@ -1,27 +1,14 @@
 from pandas import read_csv
 from sqlalchemy import create_engine
+import skyfield
+
 #this will convert csv files into a dataframe which it will be added to sqlalchemy and that will add a sqlite db
 
-comets= read_csv("near-earth-comets.csv", encoding="ISO-8859-1")
-
-engine = create_engine('sqlite:///comets.db', echo=True)
-sqlite_connection = engine.connect()
-sqlite_table = "COMETS"
-comets.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
-
-'''
-#Meteor Table
-= read_csv("near-earth-comets.csv", encoding="ISO-8859-1")
-
-engine = create_engine('sqlite:///meteorites.db', echo=True)
-sqlite_connection = engine.connect()
-sqlite_table = "METEORITES"
-meteorites.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
-'''
+#Events database
 #Eclipses Relation
 lunareclipse= read_csv("lunar.csv", encoding="ISO-8859-1")
 
-engine = create_engine('sqlite:///eclipses.db', echo=True)
+engine = create_engine('sqlite:///celestial_events.db', echo=True)
 sqlite_connection = engine.connect()
 sqlite_table = "Lunar Eclipse"
 lunareclipse.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
@@ -29,24 +16,27 @@ lunareclipse.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
 solareclipse= read_csv("solar.csv", encoding="ISO-8859-1")
 sqlite_table = "Solar Eclipse"
 solareclipse.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
- #Asteroids
-asteroid= read_csv("impacts.csv", encoding="ISO-8859-1")
-
-engine = create_engine('sqlite:///Asteroid.db', echo=True)
+#Asteroids
+asteroid = read_csv("impacts.csv", encoding="ISO-8859-1")
+#engine = create_engine('sqlite:///Asteroid.db', echo=True)
 sqlite_connection = engine.connect()
 sqlite_table = "Potential Asteroid impacts"
 asteroid.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
-
-asteroid= read_csv("orbits.csv", encoding="ISO-8859-1")
+#Asteroid Magnitude & Diameter are present in table
+asteroid_orbits = read_csv("orbits.csv", encoding="ISO-8859-1")
 sqlite_table = "Asteroid Orbits"
-asteroid.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
+asteroid_orbits.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
 
 #Celestial Objects ~ Planets
-comets= read_csv("planets.csv", encoding="ISO-8859-1")
-
+planets= read_csv("planets.csv", encoding="ISO-8859-1")
 engine = create_engine('sqlite:///celestial_objects.db', echo=True)
 sqlite_connection = engine.connect()
-sqlite_table = "Planets"
-comets.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
+planets.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
 
+#Stars, but for now populated by the Sun only
+stars=read_csv("stars.csv",encoding="ISO-8859-1")
+engine=create_engine('sqlite:///Stars.db', echo=True)
+sqlite_table = "Stars"
+sqlite_connection = engine.connect()
+stars.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
 sqlite_connection.close()
