4999bce9448b0ee3006f2c806b27af39211f2757
itsraf <45185883+itsraf@users.noreply.github.com>
2020-07-26 11:30:47 -0400

    To fix the issues with path, use this quick patch
    Will fix the path issue in the meantime

---
+++ create_db.py
@@ -0,0 +1,52 @@
+from pandas import read_csv
+from sqlalchemy import create_engine
+import skyfield
+
+#this will convert csv files into a dataframe which it will be added to sqlalchemy and that will add a sqlite db
+
+#Events database
+#Eclipses Relation
+lunareclipse= read_csv("lunar.csv", encoding="ISO-8859-1")
+engine = create_engine('sqlite:///celestial_events.db', echo=True)
+sqlite_connection = engine.connect()
+sqlite_table = "Lunar Eclipse"
+lunareclipse.to_sql(sqlite_table, sqlite_connection, if_exists='replace')
+
+solareclipse= read_csv("solar.csv", encoding="ISO-8859-1")
+engine = create_engine('sqlite:///solar_eclipse.db', echo=True)
+sqlite_connection = engine.connect()
+sqlite_table = "Solar Eclipse"
+solareclipse.to_sql(sqlite_table, sqlite_connection, if_exists='replace')
+#Asteroids
+asteroid = read_csv("impacts.csv", encoding="ISO-8859-1")
+engine = create_engine('sqlite:///Asteroid.db', echo=True)
+sqlite_connection = engine.connect()
+sqlite_table = "Potential Asteroid impacts"
+asteroid.to_sql(sqlite_table, sqlite_connection, if_exists='replace')
+#Asteroid Magnitude & Diameter are present in table
+asteroid_orbits = read_csv("orbits.csv", encoding="ISO-8859-1")
+engine = create_engine('sqlite:///Asteroid_orbit.db', echo=True)
+sqlite_connection = engine.connect()
+sqlite_table = "Asteroid Orbits"
+asteroid_orbits.to_sql(sqlite_table, sqlite_connection, if_exists='replace')
+
+#Celestial Objects ~ Planets
+planets= read_csv("planets.csv", encoding="ISO-8859-1")
+engine = create_engine('sqlite:///celestial_objects.db', echo=True)
+sqlite_connection = engine.connect()
+sqlite_table = "celestial_objects"
+planets.to_sql(sqlite_table, sqlite_connection, if_exists='replace')
+#Comets
+comets= read_csv("near-earth-comets.csv", encoding="ISO-8859-1")
+engine = create_engine('sqlite:///near_earth_comets.db', echo=True)
+sqlite_connection = engine.connect()
+sqlite_table = "Comets"
+comets.to_sql(sqlite_table, sqlite_connection, if_exists='replace')
+
+#Stars, but for now populated by the Sun only
+stars=read_csv("stars.csv",encoding="ISO-8859-1")
+engine=create_engine('sqlite:///Stars.db', echo=True)
+sqlite_connection = engine.connect()
+sqlite_table = "Stars"
+stars.to_sql(sqlite_table, sqlite_connection, if_exists='replace')
+sqlite_connection.close()
